9b3ad449c5d244cb000a24034aa1c1f83f3ee91b
Lucas Oliveira Bueno <lucasgalvaooliveira12@gmail.com>
2024-03-13 17:06:19 -0400

    testando funcionalidades do breakpoint com while

--- Conceitos Basicos - Estrutura de Repetição e Coleções/Conceitos Basicos - Estrutura de Repetição e Coleções/main.swift
+++ Conceitos Basicos - Estrutura de Repetição e Coleções/Conceitos Basicos - Estrutura de Repetição e Coleções/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: Estrutura de Repetição
 
+// MARK: While
+// no codigo abaixo index é igual a zero
+var index: Int = 0
+
+//enquanto index nao for igual ou menor que 80 ele ira continuar adicionando + 1 até sair do ponto de repetição
+while index < 80 {
+    print("index: \(index)")
+    index = index + 1
+}
+
+print("O valor final do index é : \(index)")

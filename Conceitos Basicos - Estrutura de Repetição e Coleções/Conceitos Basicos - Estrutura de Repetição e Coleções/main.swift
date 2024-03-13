64f36a6642bf4b3a080722f5da53f8a37b741bba
Lucas Oliveira Bueno <lucasgalvaooliveira12@gmail.com>
2024-03-13 17:48:31 -0400

    utilizando repeat junto com while

--- Conceitos Basicos - Estrutura de Repetição e Coleções/Conceitos Basicos - Estrutura de Repetição e Coleções/main.swift
+++ Conceitos Basicos - Estrutura de Repetição e Coleções/Conceitos Basicos - Estrutura de Repetição e Coleções/main.swift
@@ -10,6 +10,7 @@ import Foundation
 // MARK: Estrutura de Repetição
 
 // MARK: While
+// As condicoes são usadas para verificação, sao o mesmo que booleano de verdadeiro ou falso
 // no codigo abaixo index é igual a zero
 var index: Int = 0
 
@@ -20,3 +21,28 @@ while index < 80 {
 }
 
 print("O valor final do index é : \(index)")
+
+// Outro exemplo
+// O While primeiro questiona e depois executa a condição 
+
+var idade:Int = 0
+
+while idade < 18 {
+    print("Voce é menor de idade tem apenas: \(idade) anos")
+    idade = idade + 1
+}
+
+print("Agora você é maior de idade já tem \(idade) anos")
+
+// MARK: - REPEAT
+
+// Faz a mesma coisa do while, ou faz um dentro do outro.
+
+var indexRepeat: Int = 0
+
+// A unica diferença entre o while é na hora da validação, executando primeiro para depois ver a condição
+// Ou seja, primeiro ele executa e depois questiona, já no while ele questiona primeiro e depois executa.
+repeat {
+    print("index: \(indexRepeat)")
+    indexRepeat = indexRepeat + 1
+} while indexRepeat <= 3

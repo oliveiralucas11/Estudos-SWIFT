012ac50daf5de70cc3d80c2a25cf9a2ed11232b3
Lucas Oliveira Bueno <lucasgalvaooliveira12@gmail.com>
2024-03-13 16:28:50 -0400

    estrutura de repeticao e colecoes

---
+++ Conceitos Basicos - Estrutura de Repetição e Coleções/Conceitos Basicos - Estrutura de Repetição e Coleções/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Conceitos Basicos - Estrutura de Repetição e Coleções
+//
+//  Created by Lucas Oliveira Bueno on 13/03/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+

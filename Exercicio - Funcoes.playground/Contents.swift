960fe25b8a600715769ed637ec1894bd4f8c56f7
Lucas Oliveira Bueno <lucasgalvaooliveira12@gmail.com>
2024-03-13 14:43:13 -0400

    mais exericicios de funcoes

--- Exercicio - Funcoes.playground/Contents.swift
+++ Exercicio - Funcoes.playground/Contents.swift
@@ -4,26 +4,25 @@ import UIKit
 
 //MARK: - 1) Criar uma função com o nome soma que tenha 3 parametros (valor1:Int, valor2:Double, valor3:Int) e que ele faça uma soma entre os valores e quero um int de retorno sob o valor total.
 
-
-func soma (valor1: Int, valor2: Double, valor3: Int) -> Int {
-    return valor1 + Int(valor2) + valor3
+func soma (valor1: Int, valor2: Double, valor3: Double) -> Double {
+    return Double(valor1) + valor2 + valor3
 }
 
-let resultadoSoma: Int = soma(valor1: 10, valor2: 20.20, valor3: 30)
-print("A soma total entre os valores é de \(resultadoSoma)")
+let valorTotal: Double = soma(valor1: 30, valor2: 22.3, valor3: 50.3)
+print("O valor total ta soma é de: \(valorTotal)")
 
 
 //MARK: - 2) Crie uma função onde exiba na area de debug uma seguinte frase "Olá mundo, estou aprendendo a criar minha primeiras funções".
 
-func exibirMensagem () {
+func exibirFrase () {
     print("Olá mundo, estou aprendendo a criar minhas primeiras funções")
 }
 
-exibirMensagem()
+exibirFrase()
 
-func exibirMensagem2 () -> String {
-    return "Olá mundo estou aprendendo a criar minha primeiras funções"
+func exibirFrase2 () -> String {
+    return "Ola mundo, estou aprendendo a criar minha primeiras funcoes"
 }
 
-var retornandoMensagem: String = exibirMensagem2()
-print(retornandoMensagem)
+let debugarFrase: String = exibirFrase2()
+print(debugarFrase)

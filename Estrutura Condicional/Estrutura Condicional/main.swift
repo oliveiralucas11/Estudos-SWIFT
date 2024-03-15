e64bff5ca33cac1b969bf2f64172e631d193acee
Lucas Oliveira Bueno <lucasgalvaooliveira12@gmail.com>
2024-03-15 15:15:23 -0400

    treinando mais condicionais

--- Estrutura Condicional/Estrutura Condicional/main.swift
+++ Estrutura Condicional/Estrutura Condicional/main.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 // MARK: - Estrutura Condicional
+// Condição é sempre SIM ou NAO
 
 // Passo a passo
 
@@ -15,3 +16,30 @@ import Foundation
 // Tudo que está a frente do "if" significa que está realizando uma validação se aquilo é VERDADEIRO ou FALSO.
 
 // EXEMPLO
+
+// if 1 + 1 == 2 {
+//    se entrar nesse bloco de código, significa que a CONDIÇAO é VERDADEIRA
+// }
+
+// Porem, não podemos sempre prever apenas casos verdadeiros e sim casos falsos, com isso utilizamos o ELSE"
+
+// Exemplo
+
+// if 1 + 1 == 2 {
+//    se entrar nesse bloco de código, significa que a CONDIÇAO é VERDADEIRA
+// } else {
+//  se cair nesse bloco significa que a CONDICAO é FALSA
+// }
+
+
+
+func eMaiorIdade(idade : Int) -> Bool {
+        if idade >= 18 {
+        return true
+    } else {
+        return false
+    }
+}
+
+var maiorIdade: Bool = eMaiorIdade(idade: 30)
+print(maiorIdade)

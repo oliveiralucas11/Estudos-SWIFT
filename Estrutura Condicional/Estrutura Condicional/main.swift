dbf088a1070fbd26cf7bf7bdfb5f02e30e6f6527
Lucas Oliveira Bueno <lucasgalvaooliveira12@gmail.com>
2024-03-15 00:55:21 -0400

    anotacoes inicias sobre estrutura condicional

--- Estrutura Condicional/Estrutura Condicional/main.swift
+++ Estrutura Condicional/Estrutura Condicional/main.swift
@@ -7,4 +7,11 @@
 
 import Foundation
 
+// MARK: - Estrutura Condicional
 
+// Passo a passo
+
+// if -> Significa "SE"
+// Tudo que está a frente do "if" significa que está realizando uma validação se aquilo é VERDADEIRO ou FALSO.
+
+// EXEMPLO

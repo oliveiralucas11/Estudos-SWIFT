70987a0760d8bf7abdbdfafc57671dc09abb086d
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-08-03 17:01:27 -0400

    criei exemplos de classe com construtores e apliquei o conceito de referencia

--- OrientaçãoObjetos-classe/OrientaçãoObjetos-classe/main.swift
+++ OrientaçãoObjetos-classe/OrientaçãoObjetos-classe/main.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+//MARK: - CLASSES
+// Nada mais é que um esboço de um objeto que contem caracteristicas e ações sendo elas variaveis, constantes e tambem suas ações/metodos
+// Outra principal caracteristica é que a classe trabalha com reference type (referencia)
+
 // Um Objeto ele tem CARACTERISTICAS e AÇOES
 
 // Caracteristicas de um objeto -> chamadas de ATRIBUTOS ou PROPRIEDADES
@@ -14,6 +18,7 @@ import Foundation
 
 // Para criar uma classe devemos começar com a palavra reservada "class"
 // Diferente de uma variavel onde usamos letra minuscula e depois a maiuscula na class a primeira letra deve sempre ser maiuscula
+// sendo tambem uma classe estatica, com valor definido.
 
 class Automovel {
     
@@ -91,4 +96,81 @@ class Fogao {
     }
     
 }
+
+//var meuCarro: Automovel? // Podemos criar uma variavel do tipo objeto pegar dele todas as caracteristicas e ações do objeto
+
+var meuCarro: Automovel = Automovel() // A partir do momento que colocamos Automovel() estamos dando vida ao objeto
+// Dessa forma estamos estanciando nosso objeto , sem isso a linguagem só entende que temos as caracteristicas mas não os valores, precisando ser instanciado pra sabermos os valores e acessar as acoes do objeto.
+// Sem estar instanciando o objeto está inativo retornando um valor Nil.
+
+print(meuCarro.cor)
+print(meuCarro.isEletrico)
+print(meuCarro.marca)
+meuCarro.ligarFarol()
+meuCarro.ligarOCarro()
+
+meuCarro.cor = "Branco"
+print(meuCarro.cor)
+
+var carroBernardo: Automovel = Automovel()
+carroBernardo.marca = "Porsche"
+
+var carroEduardo: Automovel = Automovel()
+carroEduardo.cor = "Amarelo"
+
+
+// MARK: - Construtores
+
+// Diferente da classe normal nós não temos o valor definido
+// Sendo muito parecido com uma função com parametro usando o "init" e com isso indicamos TODOS OS VALORES DAS VARIAVEIS OU CONSTANTES QUE NAO SE INICIAM COM VALOR
+// e tambem sendo uma classe dinamica
+
+
+class Pessoa {
+    
+    var nome: String
+    var altura: Double
+    var peso: Double
+    
+    init(nome: String, altura: Double, peso: Double) {
+        self.nome = nome // Aqui estamos dizendo que a minha variavel nome que não teve valor definido, irá nascer com o mesmo nome da variavel
+        self.altura = altura
+        self.peso = peso
+    }
+}
+
+
+var caio: Pessoa = Pessoa(nome: "Caio", altura: 1.84, peso: 84) // Diferente da classe normal quando instanciamos uma classe com construtores, ele nos obriga a já informarmos os respectivos valores
+var matheus: Pessoa = Pessoa(nome: "Matheus", altura: 1.65, peso: 69)
+//print(caio.nome)
+//print(caio.peso)
+//print(caio.altura)
+
+caio.altura = 1.90
+
+
+// Referencia uma das principais caracteristicas de uma classe
+// Podemos atribuir o valor de um objeto a outro como no ex abaixo a partir da linha 164
+
+print(caio.nome)
+print(matheus.nome)
+
+caio.nome = "Caio"
+matheus.nome = "Matheus Souza"
+
+print(caio.nome)
+print(matheus.nome)
+
+// caio recebe todas as caracteristicas de matheus
+caio = matheus // Aqui estou atribuindo a caio todo o valor contido em matheus
+
+print(caio.nome)
+print(matheus.nome)
+
+// Como caio é = matheus o valor também e alterado em matheus ele se torna um só
+caio.nome = "Alberto"
+
+print(caio.nome)
+print(matheus.nome)
+
 

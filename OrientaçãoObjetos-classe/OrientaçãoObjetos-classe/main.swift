aa5eb19a0a8b5043e651b15fcf1c8d4433786a33
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-08-02 21:43:32 -0400

    mais exemplos praticando objetos com caracteristicas e ações

--- OrientaçãoObjetos-classe/OrientaçãoObjetos-classe/main.swift
+++ OrientaçãoObjetos-classe/OrientaçãoObjetos-classe/main.swift
@@ -36,3 +36,59 @@ class Automovel {
     
 }
 
+class Forno {
+    
+    var cor: String = "Prata"
+    var marca: String = "Dako"
+    var voltagem: Int = 220
+    var capacidade: Double = 56.2
+    var isAGas: Bool = false
+    
+    
+    func ligarForno () {
+        print("Forno Ligado")
+    }
+    
+    func ajustarTemperatura () {
+        print("Ajustado a 220˚C")
+    }
+    
+}
+
+class Aviao {
+    
+    var cor: String = "Vermelho"
+    var marca: String = "Boeing"
+    var tipo: Int = 738
+    var isTurboHelice: Bool = false
+    var isTurboFan: Bool = true
+    
+    func ligarAvionicos () {
+        print("Avionicos Ligados")
+    }
+    
+    func darPartidaNoMotor1 () {
+        print("Acionando Motor 1")
+        
+    }
+    
+}
+
+class Fogao {
+    
+    var voltagem: Int = 110
+    var isGas: Bool = true
+    var eEletrico: Bool = false
+    var quantidadeDeBocas: Int = 4
+    var tamanhoDoForno: Double = 82.2
+    
+    func acenderChama () {
+        print("Chama Acesa")
+    }
+    
+    func diminuirIntensidade {
+        print("Intensidade diminuida para 50%")
+    }
+    
+}
+

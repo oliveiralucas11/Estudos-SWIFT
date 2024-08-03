c4c5bbfec3bac278993282628cbc26e0d2e71faf
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-08-02 21:15:58 -0400

    criando as primeiras class para entender seu funcionamento

--- OrientaçãoObjetos-classe/OrientaçãoObjetos-classe/main.swift
+++ OrientaçãoObjetos-classe/OrientaçãoObjetos-classe/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
+// Um Objeto ele tem CARACTERISTICAS e AÇOES
 
+// Caracteristicas de um objeto -> chamadas de ATRIBUTOS ou PROPRIEDADES
+// AÇOES = FUNÇAO ou METODO
+
+// Para criar uma classe devemos começar com a palavra reservada "class"
+// Diferente de uma variavel onde usamos letra minuscula e depois a maiuscula na class a primeira letra deve sempre ser maiuscula
+
+class Automovel {
+    
+    var cor: String = "Preto"
+    var numeroDePortas: Int = 2
+    var quantidadeDeRodas: Int = 4
+    var marca: String = "Honda"
+    var motor: Double = 1.0
+    var isEletrico: Bool = true
+    var quantidadeDePassageiros: Int = 5
+    
+    func ligarFarol () {
+        print("O farol está ligado")
+    }
+    
+    func ligarOCarro () {
+        print("O carro está ligado")
+    }
+    
+    
+}
 
